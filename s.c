6ebf7135b85fd7ce0bfd21b5d6de99c6c3d7a031
karthikakeyan <karthikakeyan@users.noreply.github.com>
2016-08-24 10:09:51 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int no;
+    printf("1.sunday 2.monday 3.tuesday 4.wednesday 5.thursday 6.friday 7.saturday\n");
+    scanf("%d",&no);
+    switch(n)
+    {
+        case 1:printf("False");
+        break;
+        case 2:printf("True");
+        break;
+        case 3:printf("True");
+        break;
+        case 4:printf("True");
+        break;
+        case 5:printf("True");
+        break;
+        case 6:printf("True");
+        break;
+        case 7:printf("True");
+        break;
+    }
+    return 0;
+}
